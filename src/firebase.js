eefaa9b2cf879ae11af63e99d569c63e5484bad7
Samiya <skmcintosh20@gmail.com>
2022-04-06 19:21:15 -0700

    intialize macsapp

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyD5ndh_hGtKc2no4vouiEYS-726a-3b2zI",
+	authDomain: "macsapp-7dfb8.firebaseapp.com",
+	projectId: "macsapp-7dfb8",
+	storageBucket: "macsapp-7dfb8.appspot.com",
+	messagingSenderId: "760897773348",
+	appId: "1:760897773348:web:6baa3fb3793a9f71829db1",
+	measurementId: "G-JRK3T985MF",
+};
