f33ddb0b68ea0c63ae9712df1321e2b876b43437
Samiya <skmcintosh20@gmail.com>
2022-04-07 19:23:46 -0700

    Added axios and the back end

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Line from "./components/Line";
 import Sidebar from "./components/Sidebar";
+import Pusher from "pusher-js";
+import axios from "./axios";
 
 function App() {
+	const [messages, setMessages] = useState([]);
+
+	useEffect(() => {
+		axios.get("/messages/sync").then((response) => {
+			setMessages(response.data);
+		});
+	}, []);
+
+	useEffect(() => {
+		const pusher = new Pusher("d6e0e5636b5fe01fa334", {
+			cluster: "us2",
+		});
+
+		const channel = pusher.subscribe("messages");
+		channel.bind("inserted", (newMessage) => {
+			alert(JSON.stringify(newMessage));
+			setMessages([...messages, newMessage]);
+		});
+
+		return () => {
+			channel.unbind_all();
+			channel.unsubscribe();
+		};
+	}, [messages]);
+
+	console.log(messages);
 	return (
 		<div className="App">
 			<div className="app_body">
 				{/* Sidebar */}
 				<Sidebar />
 				{/* ChatLine */}
-				<Line />
+				<Line messages={messages} />
 			</div>
 		</div>
 	);
--- src/components/Line.js
+++ src/components/Line.js
@@ -1,13 +1,28 @@
 import { Avatar, IconButton } from "@material-ui/core";
-import React from "react";
+import React, { useState } from "react";
 import "../Line.css";
 import RemoveRedEyeIcon from "@material-ui/icons/RemoveRedEye";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import AttachFileIcon from "@material-ui/icons/AttachFile";
 import MicIcon from "@material-ui/icons/Mic";
 import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import axios from "../axios";
 
-function Line() {
+function Line({ messages }) {
+	const [input, setInput] = useState("");
+
+	const sendMessage = async (e) => {
+		e.preventDefault(); //stops refresh
+		await axios.post("/messages/new", {
+			message: input,
+			name: "Gear Room",
+			timestamp: "Just Now!",
+			received: true,
+		});
+
+		setInput("");
+	};
+
 	return (
 		<div className="line">
 			<div className="line_header">
@@ -29,22 +44,24 @@ function Line() {
 				</div>
 			</div>
 			<div className="line_body">
-				<p className="line_text">
-					<span className="line_name">Mac</span>
-					Mopars where its at
-					<span className="line_timestamp">{new Date().toUTCString()}</span>
-				</p>
-				<p className="line_text line_receiver">
-					<span className="line_name">Mac</span>
-					Mopars where its at
-					<span className="line_timestamp">{new Date().toUTCString()}</span>
-				</p>
+				{messages.map((message) => (
+					<p className={`line_text ${message.received && "line_receiver"}`}>
+						<span className="line_name">{message.name}</span>
+						{message.message}
+						<span className="line_timestamp">{message.timestamp}</span>
+					</p>
+				))}
 			</div>
 			<div className="line_footer">
 				<InsertEmoticonIcon />
 				<form>
-					<input placeholder="Type a message" type="text" />
-					<button className="footer_btn" type="submit">
+					<input
+						value={input}
+						onChange={(e) => setInput(e.target.value)}
+						placeholder="Type a message"
+						type="text"
+					/>
+					<button onClick={sendMessage} className="footer_btn" type="submit">
 						Send A Dime
 					</button>
 				</form>
